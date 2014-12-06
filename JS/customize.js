f577121f602bed047b1661d3d43e9fd24dc4a3ca
stephan.detje <stephan.detje@student.hpi.uni-potsdam.de>
2014-12-06 13:55:32 +0100

    adjusted colorscheme interface

--- JS/customize.js
+++ JS/customize.js
@@ -12,6 +12,8 @@
 
 var customize = function(config){
 
+    /*** variables ***/
+
     var defaultColorScheme = [{
         'background-color': 'white',
         'color': 'black'
@@ -19,16 +21,35 @@ var customize = function(config){
         'background-color': 'black',
         'color': 'white'
     }],
-       schemeDefined = false
+        schemeDefined = false,
+        colorScheme = []
+
+    /*** config ***/
 
     this.setConfig = function (config) {
         if(typeof config.colorScheme != 'undefined'){
-            this.colorScheme = config.colorScheme
+            config.colorScheme.forEach(function(arg){
+                colorScheme.push({
+                    'background-color': arg[0],
+                    'color': arg[1]
+                })
+            })
+
             schemeDefined = true
         }
     }
 
+    /*** customizing colors ***/
+
     this.setDefaultColorSchemeForClass = function(c1, c2){
+        jss.set("." + c1, defaultColorScheme[0])
+        if(typeof c2 != 'undefined')
+            jss.set("." + c2, defaultColorScheme[1])
+
+        return this
+    }
+
+    this.setColorSchemeForClass = function(classes){
         var scheme
 
         if(schemeDefined)
@@ -36,10 +57,12 @@ var customize = function(config){
         else
             scheme = defaultColorScheme
 
-
-        jss.set("." + c1, scheme[0])
-        if(typeof c2 != 'undefined')
-            jss.set("." + c2, scheme[1])
+        classes.forEach(function(arg, i){
+            if(i < scheme.length)
+                jss.set("." + arg, scheme[i])
+            else
+                console.log("Not enough color schemes available to be set...")
+        })
 
         return this
     }
