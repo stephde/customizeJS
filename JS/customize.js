9d60e5a023acfe43b6e404d8c780030248fc602d
stephan.detje <stephan.detje@student.hpi.uni-potsdam.de>
2014-12-06 13:33:25 +0100

    adjusted example, enabled color setting

--- JS/customize.js
+++ JS/customize.js
@@ -18,17 +18,30 @@ var customize = function(config){
     }, {
         'background-color': 'black',
         'color': 'white'
-    }]
+    }],
+       schemeDefined = false
 
     this.setConfig = function (config) {
-
+        if(typeof config.colorScheme != 'undefined'){
+            this.colorScheme = config.colorScheme
+            schemeDefined = true
+        }
     }
 
     this.setDefaultColorSchemeForClass = function(c1, c2){
-        jss.set("." + c1, defaultColorScheme[0])
+        var scheme
 
+        if(schemeDefined)
+            scheme  = colorScheme
+        else
+            scheme = defaultColorScheme
+
+
+        jss.set("." + c1, scheme[0])
         if(typeof c2 != 'undefined')
-            jss.set("." + c2, defaultColorScheme[1])
+            jss.set("." + c2, scheme[1])
+
+        return this
     }
 
 
