63447d0cec6e4c73e4b0b00337c75d5a4c9f6788
stephan.detje <stephan.detje@student.hpi.uni-potsdam.de>
2014-12-09 00:07:39 +0100

    included Jasmine for testing

---
+++ test/test.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Stephan on 08.12.2014.
+ */
+
+describe("Customize JS test suite", function(){
+
+    it("should include customizeJS", function() {
+        expect(Customize).not.toBeUndefined()
+    })
+
+
+})
